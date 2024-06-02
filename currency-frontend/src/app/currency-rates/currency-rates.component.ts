9483bc95637c4b3f7c60d22022ab9749a7307dbb
Mathias007 <mathias.najlepszy@gmail.com>
2024-06-02 10:47:07 +0200

    [FE] Add currency-rates component structure and logic.

--- currency-frontend/src/app/currency-rates/currency-rates.component.ts
+++ currency-frontend/src/app/currency-rates/currency-rates.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CurrencyService } from '../currency.service';
 
 @Component({
   selector: 'app-currency-rates',
-  standalone: true,
-  imports: [],
   templateUrl: './currency-rates.component.html',
-  styleUrl: './currency-rates.component.scss'
+  styleUrls: ['./currency-rates.component.css']
 })
-export class CurrencyRatesComponent {
+export class CurrencyRatesComponent implements OnInit {
+  rates: any;
 
+  constructor(private currencyService: CurrencyService) { }
+
+  ngOnInit(): void {
+    this.currencyService.getRates().subscribe((data: any) => {
+      this.rates = data;
+    });
+  }
 }
