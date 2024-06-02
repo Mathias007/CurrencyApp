17517e912691db105145971f5aba4f699a6ab357
Mathias007 <mathias.najlepszy@gmail.com>
2024-06-02 12:05:23 +0200

    [BE] Prepare tests for currency_backend api.

--- currency_backend/api/tests.py
+++ currency_backend/api/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import CurrencyRate
 
-# Create your tests here.
+class CurrencyRateTestCase(TestCase):
+    def setUp(self):
+        CurrencyRate.objects.create(date='2023-01-01', value=4.1234)
+
+    def test_currency_rate(self):
+        rate = CurrencyRate.objects.get(date='2023-01-01')
+        self.assertEqual(rate.value, 4.1234)
