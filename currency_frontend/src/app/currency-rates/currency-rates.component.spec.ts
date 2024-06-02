360fbba9a80640a98fa05b5602fcb666768e5b84
Mathias007 <mathias.najlepszy@gmail.com>
2024-06-02 12:05:36 +0200

    [FE] Prepare tests for currency-rates component.

--- currency_frontend/src/app/currency-rates/currency-rates.component.spec.ts
+++ currency_frontend/src/app/currency-rates/currency-rates.component.spec.ts
@@ -1,23 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CurrencyRatesComponent } from './currency-rates.component';
 
 describe('CurrencyRatesComponent', () => {
-  let component: CurrencyRatesComponent;
-  let fixture: ComponentFixture<CurrencyRatesComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CurrencyRatesComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(CurrencyRatesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      declarations: [ CurrencyRatesComponent ],
+      imports: [ HttpClientTestingModule ]
+    }).compileComponents();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    const fixture = TestBed.createComponent(CurrencyRatesComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
 });
