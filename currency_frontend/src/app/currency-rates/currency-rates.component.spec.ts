b59a76fa6a48c411e35c6e2ea1cc4b4ce2a6f5db
Mathias007 <mathias.najlepszy@gmail.com>
2024-06-03 21:40:36 +0200

    [FE] Update unit tests.

--- currency_frontend/src/app/currency-rates/currency-rates.component.spec.ts
+++ currency_frontend/src/app/currency-rates/currency-rates.component.spec.ts
@@ -1,28 +1,79 @@
 import { TestBed } from '@angular/core/testing';
 import { CurrencyRatesComponent } from './currency-rates.component';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { CurrencyService } from '../currency.service';
+import { of } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
-describe('AppComponent', () => {
+describe('CurrencyRatesComponent', () => {
+  let component: CurrencyRatesComponent;
+  let fixture: any;
   let httpTestingController: HttpTestingController;
+  let currencyService: CurrencyService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      declarations: [CurrencyRatesComponent],
-    });
+      imports: [
+        HttpClientTestingModule, 
+        CommonModule, 
+        CurrencyRatesComponent
+      ],
+      providers: [CurrencyService],
+    }).compileComponents();
 
     httpTestingController = TestBed.inject(HttpTestingController);
+    fixture = TestBed.createComponent(CurrencyRatesComponent);
+    component = fixture.componentInstance;
+    currencyService = TestBed.inject(CurrencyService); 
   });
 
-  it('should fetch rates on button click', () => {
-    const fixture = TestBed.createComponent(CurrencyRatesComponent);
-    const app = fixture.componentInstance;
-    app.downloadCurrencyRates();
-    
-    const req = httpTestingController.expectOne('http://localhost:8000/download-currency-rates/');
-    expect(req.request.method).toEqual('POST');
-    req.flush({});
 
+  afterEach(() => {
     httpTestingController.verify();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should download currency rates', () => {
+    spyOn(currencyService, 'downloadCurrencyRates').and.returnValue(of({}));
+
+    component.downloadCurrencyRates();
+    expect(currencyService.downloadCurrencyRates).toHaveBeenCalled();
+  });
+
+  it('should fetch currency rates by period', () => {
+    const mockRates = [
+      { period: '2023-01-01', code: 'USD', avg_value: 3.8 },
+      { period: '2023-02-01', code: 'USD', avg_value: 3.9 }
+    ];
+    spyOn(currencyService, 'getRatesByPeriod').and.returnValue(of(mockRates));
+
+    component.fetchCurrencyRatesByPeriod('month');
+    expect(currencyService.getRatesByPeriod).toHaveBeenCalledWith('month');
+    expect(component.currencyRates).toEqual(component.transformRates(mockRates, 'month'));
+  });
+
+  it('should fetch all currency rates', () => {
+    const mockRates = [
+      { period: '2023-01-01', code: 'USD', avg_value: 3.8 },
+      { period: '2023-02-01', code: 'USD', avg_value: 3.9 }
+    ];
+    spyOn(currencyService, 'getAllRates').and.returnValue(of(mockRates));
+
+    component.fetchAllCurrencyRates();
+    expect(currencyService.getAllRates).toHaveBeenCalled();
+    expect(component.currencyRates).toEqual(component.transformRates(mockRates, 'general'));
+  });
+
+  it('should transform rates correctly', () => {
+    const rates = [
+      { period: '2023-03-15', code: 'USD', avg_value: 4.2 },
+      { period: '2023-01-10', code: 'EUR', avg_value: 4.6 }
+    ];
+    const transformed = component.transformRates(rates, 'year');
+    expect(transformed[0].transformedPeriod).toEqual('2023');
+    expect(transformed[1].transformedPeriod).toEqual('');
   });
 });
--- currency_frontend/src/app/currency.service.spec.ts
+++ currency_frontend/src/app/currency.service.spec.ts
@@ -1,16 +1,56 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CurrencyService } from './currency.service';
 
 describe('CurrencyService', () => {
   let service: CurrencyService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CurrencyService]
+    });
     service = TestBed.inject(CurrencyService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should download currency rates', () => {
+    service.downloadCurrencyRates().subscribe(response => {
+      expect(response).toBeTruthy();
+    });
+
+    const req = httpTestingController.expectOne('http://localhost:8000/download-currency-rates/');
+    expect(req.request.method).toBe('POST');
+    req.flush({});
+  });
+
+  it('should get rates by period', () => {
+    const period = 'year';
+    service.getRatesByPeriod(period).subscribe(response => {
+      expect(response).toBeTruthy();
+    });
+
+    const req = httpTestingController.expectOne(`http://localhost:8000/fetch-currency-rates-period/${period}/`);
+    expect(req.request.method).toBe('GET');
+    req.flush({});
+  });
+
+  it('should get all rates', () => {
+    service.getAllRates().subscribe(response => {
+      expect(response).toBeTruthy();
+    });
+
+    const req = httpTestingController.expectOne('http://localhost:8000/fetch-currency-rates-general/');
+    expect(req.request.method).toBe('GET');
+    req.flush({});
   });
 });
