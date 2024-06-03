3ae2fb2e1be1a5cdad6a914ac848c903a88cd780
Mathias007 <mathias.najlepszy@gmail.com>
2024-06-03 21:14:15 +0200

    [FE] Remove unused example angular code and customize the rest.

--- currency_frontend/src/app/app.component.spec.ts
+++ currency_frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'currency-frontend' title`, () => {
+  it(`should have the 'CurrencyAppClient' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('currency-frontend');
+    expect(app.title).toEqual('CurrencyAppClient');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, currency-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Witamy w CurrencyAppClient!');
   });
 });
