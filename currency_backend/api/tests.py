83284ed21bb1c57517429892ae9b1bfe2214d770
Mathias007 <mathias.najlepszy@gmail.com>
2024-06-03 21:40:52 +0200

    [BE] Update unit tests.

--- currency_backend/api/tests.py
+++ currency_backend/api/tests.py
@@ -9,7 +9,7 @@ class CurrencyRateTestCase(TestCase):
 
     def test_currency_rate(self):
         rate = CurrencyRate.objects.get(date='2023-01-01')
-        self.assertEqual(rate.value, 4.1234)
+        self.assertEqual(float(rate.value), 4.1234)
 
     def test_create_currency_rate(self):
         rate = CurrencyRate.objects.create(date=date.today(), currency='USD', code='USD', value=4.5)
